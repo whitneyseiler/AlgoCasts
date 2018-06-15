f3b179ae33cfe940db57a4b61bd2ff61fd30f79d
Whitney Seiler <whitney.seiler@gmail.com>
2018-06-15 12:51:15 -0700

    Pass MaxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let cache = {};
+
+    for (var i = 0; i < str.length; i++) {
+        if (cache[str[i]]) {
+            cache[str[i]]++;
+        } else {
+            cache[str[i]] = 1;
+        }
+    }
+
+    return Object.keys(cache).sort()[0]
+}
 
 module.exports = maxChar;
